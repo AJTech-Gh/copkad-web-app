d49fcf3feef251c47a9d8e491ff1a627aaf22479
Fiifi1 <freshprince708@yahoo.com>
2020-08-06 21:23:38 +0000

    add user done

--- app.py
+++ app.py
@@ -12,5 +12,5 @@ mail = Mail(app)
 
 import routes, models
 
-# if __name__ == "__main__":
-#     app.run(host='127.0.0.1', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
